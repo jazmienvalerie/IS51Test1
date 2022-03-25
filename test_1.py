f84bb852565c3c3331eb44b13cacdc1deec73578
Jazmien <jazmienb247@gmail.com>
2022-03-25 08:51:06 -0700

    finished structured english

---
+++ test_1.py
@@ -0,0 +1,17 @@
+
+
+"""
+This program wants to determine which payment option is better out of the two payment options.
+The first option consists of $100 per day for 10 days. The second option consists of 1 dollar per day 
+with the dollar amount being doubled each day for 10 days. This program will use two functions to calculate the 
+amount of money earned. function1 will be used to calculate the amount for option1 and function2 will be used 
+to calculate the amount for option2.
+
+funtion1 will output and calculate $100 * 10 days.
+function2 will loop 10 times, as the amount doubles with each time; and then the amount is added to the total.
+
+If Option 1 is better, then the given output will be "Option 1 is better".
+If Option 2 is better, then the given output will be "Option 2 is better".
+If the amount is equal or the same, the given output will be "Option 1 and Option 2 pays the same".
+
+"""
